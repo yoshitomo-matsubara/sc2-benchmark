6dcd1051b89a20f499a45232dc819db6d7027624
Yoshitomo Matsubara <yoshitom@uci.edu>
2021-11-02 16:23:21 -0700

    added docstrings

--- sc2bench/transforms/codec.py
+++ sc2bench/transforms/codec.py
@@ -1,5 +1,4 @@
 import os
-import time
 from io import BytesIO
 from tempfile import mkstemp
 
@@ -21,6 +20,12 @@ def register_codec_module(cls):
 
 @register_codec_module
 class WrappedResize(Resize):
+    """
+    `Resize` in torchvision wrapped to be defined by `interpolation` as a str object
+    Args:
+        interpolation (str or None): Desired interpolation mode (`nearest`, `bicubic`, `bilinear`, `box`, `hamming`, `lanczos`)
+        kwargs (dict): kwargs for `Resize` in torchvision
+    """
     MODE_DICT = {
         'nearest': InterpolationMode.NEAREST,
         'bicubic': InterpolationMode.BICUBIC,
@@ -30,20 +35,33 @@ class WrappedResize(Resize):
         'lanczos': InterpolationMode.LANCZOS
     }
 
-    def __init__(self, **kwargs):
-        interpolation = kwargs.pop('interpolation', None)
-        interpolation = self.MODE_DICT.get(interpolation, None)
+    def __init__(self, interpolation=None, **kwargs):
+        if interpolation is not None:
+            interpolation = self.MODE_DICT.get(interpolation, None)
         super().__init__(**kwargs, interpolation=interpolation)
 
 
 @register_codec_module
 class PillowImageModule(nn.Module):
+    """
+    Generalized Pillow module to compress (decompress) images e.g., as part of transform pipeline.
+    Args:
+        open_kwargs (dict or None): kwargs to be used as part of Image.open(img_buffer, **open_kwargs).
+        save_kwargs (dict or None): kwargs to be used as part of Image.save(img_buffer, **save_kwargs).
+    """
     def __init__(self, open_kwargs=None, **save_kwargs):
         super().__init__()
         self.open_kwargs = open_kwargs if isinstance(open_kwargs, dict) else dict()
         self.save_kwargs = save_kwargs
 
-    def __call__(self, pil_img):
+    def forward(self, pil_img):
+        """
+        Args:
+            pil_img (PIL Image): Image to be transformed.
+
+        Returns:
+            PIL Image: Affine transformed image.
+        """
         img_buffer = BytesIO()
         pil_img.save(img_buffer, **self.save_kwargs)
         return Image.open(img_buffer, **self.open_kwargs)
@@ -52,12 +70,21 @@ class PillowImageModule(nn.Module):
 @register_codec_module
 class BpgModule(nn.Module):
     """
+    Generalized Pillow module to compress (decompress) images e.g., as part of transform pipeline.
     Modified https://github.com/InterDigitalInc/CompressAI/blob/master/compressai/utils/bench/codecs.py
+    Args:
+        encoder_path (str): file path of BPG encoder you manually installed.
+        decoder_path (str): file path of BPG decoder you manually installed.
+        color_mode (str): color mode ("ycbcr" or "rgb").
+        encoder (str): encoder type ("x265" or "jctvc").
+        subsampling_mode (str or int): subsampling mode (420 or 444).
+        bit_depth (str or int): bit depth (8 or 10).
+        quality (int): quality value in range [0, 51].
     """
     fmt = '.bpg'
 
     def __init__(self, encoder_path, decoder_path, color_mode='ycbcr', encoder='x265',
-                 subsampling_mode='444', bit_depth='8', bpg_quality=50):
+                 subsampling_mode='444', bit_depth='8', quality=50):
         super().__init__()
         if not isinstance(subsampling_mode, str):
             subsampling_mode = str(subsampling_mode)
@@ -77,8 +104,8 @@ class BpgModule(nn.Module):
         if bit_depth not in ['8', '10']:
             raise ValueError(f'Invalid bit depth value: `{bit_depth}`, which should be either 8 or 10')
 
-        if not 0 <= bpg_quality <= 51:
-            raise ValueError(f'Invalid bpg quality value: `{bpg_quality}`, which should be between 0 and 51')
+        if not 0 <= quality <= 51:
+            raise ValueError(f'Invalid quality value: `{quality}`, which should be between 0 and 51')
 
         self.encoder_path = os.path.expanduser(encoder_path)
         self.decoder_path = os.path.expanduser(decoder_path)
@@ -86,7 +113,7 @@ class BpgModule(nn.Module):
         self.encoder = encoder
         self.subsampling_mode = subsampling_mode
         self.bit_depth = bit_depth
-        self.bpg_quality = bpg_quality
+        self.quality = quality
 
     def _get_encode_cmd(self, img_file_path, output_file_path):
         cmd = [
@@ -94,7 +121,7 @@ class BpgModule(nn.Module):
             '-o',
             output_file_path,
             '-q',
-            str(self.bpg_quality),
+            str(self.quality),
             '-f',
             self.subsampling_mode,
             '-e',
@@ -112,21 +139,23 @@ class BpgModule(nn.Module):
         return cmd
 
     def run(self, pil_img):
+        """
+        Args:
+            pil_img (PIL Image): Image to be transformed.
+
+        Returns:
+            PIL Image: Affine transformed image.
+        """
         fd_i, resized_input_filepath = mkstemp(suffix='.jpg')
         fd_r, reconst_file_path = mkstemp(suffix='.jpg')
         fd_o, output_file_path = mkstemp(suffix=self.fmt)
         pil_img.save(resized_input_filepath, 'JPEG', quality=100)
 
         # Encode
-        start = time.perf_counter()
         run_command(self._get_encode_cmd(resized_input_filepath, output_file_path))
-        enc_time = time.perf_counter() - start
-        file_size_kbyte = os.stat(output_file_path).st_size / 1024
 
         # Decode
-        start = time.perf_counter()
         run_command(self._get_decode_cmd(output_file_path, reconst_file_path))
-        dec_time = time.perf_counter() - start
 
         # Read image
         reconst_img = Image.open(reconst_file_path).convert('RGB')
@@ -136,4 +165,4 @@ class BpgModule(nn.Module):
         os.remove(reconst_file_path)
         os.close(fd_o)
         os.remove(output_file_path)
-        return reconst_img, file_size_kbyte, enc_time, dec_time
+        return reconst_img
