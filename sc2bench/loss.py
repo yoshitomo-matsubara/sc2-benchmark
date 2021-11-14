71adf4732a9719bfc8717a545614a38f260dc18e
Yoshitomo Matsubara <yoshitom@uci.edu>
2021-11-14 12:03:19 -0800

    deleted an unnecessary class

--- sc2bench/loss.py
+++ sc2bench/loss.py
@@ -27,31 +27,6 @@ def extract_org_segment_loss(org_criterion, student_outputs, teacher_outputs, ta
 
 
 @register_single_loss
-class BppBasedLoss(nn.Module):
-    def __init__(self, compressor_module_path, beta, reduction='mean'):
-        super().__init__()
-        self.compressor_module_path = compressor_module_path
-        self.beta = beta
-        self.mse = nn.MSELoss(reduction=reduction)
-        self.reduction = reduction
-
-    def forward(self, student_io_dict, *args, **kwargs):
-        compressor_module_dict = student_io_dict[self.compressor_module_path]
-        compressor_inputs = compressor_module_dict['input']
-        reconstructed_inputs, likelihoods = compressor_module_dict['output']
-        reconstruction_loss = self.mse(reconstructed_inputs, compressor_inputs)
-        n, _, h, w = compressor_inputs.shape
-        num_pixels = n * h * w
-        if self.reduction == 'sum':
-            bpp = -likelihoods.log2().sum()
-        elif self.reduction == 'batchmean':
-            bpp = -likelihoods.log2().sum() / n
-        else:
-            bpp = -likelihoods.log2().sum() / num_pixels
-        return reconstruction_loss + self.beta * bpp
-
-
-@register_single_loss
 class BppLoss(nn.Module):
     def __init__(self, entropy_module_path, reduction='mean'):
         super().__init__()
@@ -60,8 +35,8 @@ class BppLoss(nn.Module):
 
     def forward(self, student_io_dict, *args, **kwargs):
         entropy_module_dict = student_io_dict[self.entropy_module_path]
-        reconstructed_inputs, likelihoods = entropy_module_dict['output']
-        n, _, h, w = reconstructed_inputs.shape
+        intermediate_features, likelihoods = entropy_module_dict['output']
+        n, _, h, w = intermediate_features.shape
         num_pixels = n * h * w
         if self.reduction == 'sum':
             bpp = -likelihoods.log2().sum()
