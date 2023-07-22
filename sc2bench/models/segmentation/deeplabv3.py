69488352fe80a85ddce2a7a06dc1185e412f747c
Yoshitomo Matsubara <yoshitom@uci.edu>
2023-07-22 11:46:37 -0700

    Add docstrings

--- sc2bench/models/segmentation/deeplabv3.py
+++ sc2bench/models/segmentation/deeplabv3.py
@@ -10,20 +10,64 @@ from ..backbone import FeatureExtractionBackbone
 from ..registry import load_classification_model
 
 
-def create_deeplabv3(backbone_model, num_input_channels=2048, uses_aux=False, num_aux_channels=1024, num_classes=21):
+def create_deeplabv3(backbone, num_input_channels=2048, uses_aux=False, num_aux_channels=1024, num_classes=21):
+    """
+    Builds DeepLabv3 model using a given updatable backbone model.
+
+    :param backbone: backbone model (usually a classification model)
+    :type backbone: nn.Module
+    :param num_input_channels: number of input channels for classification head
+    :type num_input_channels: int
+    :param uses_aux: If True, add an auxiliary branch
+    :type uses_aux: bool
+    :param num_aux_channels: number of input channels for auxiliary classification head
+    :type num_aux_channels: int
+    :param num_classes: number of output classes of the model (including the background)
+    :type num_classes: int
+    """
     aux_classifier = None
     if uses_aux:
         aux_classifier = FCNHead(num_aux_channels, num_classes)
 
     classifier = DeepLabHead(num_input_channels, num_classes)
-    return BaseSegmentationModel(backbone_model, classifier, aux_classifier)
+    return BaseSegmentationModel(backbone, classifier, aux_classifier)
 
 
 @register_segmentation_model_func
 def deeplabv3_model(backbone_config, pretrained=True, pretrained_backbone_name=None, progress=True,
                     num_input_channels=2048, uses_aux=False, num_aux_channels=1024, return_layer_dict=None,
-                    analysis_config=None, analyzable_layer_key=None, num_classes=21,
-                    start_ckpt_file_path=None, **kwargs):
+                    num_classes=21, analysis_config=None, analyzable_layer_key=None, start_ckpt_file_path=None,
+                    **kwargs):
+    """
+    Builds DeepLabv3 model using a given updatable backbone model.
+
+
+    :param backbone_config: backbone configuration
+    :type backbone_config: dict
+    :param pretrained: if True, returns a model pre-trained on COCO train2017 (torchvision)
+    :type pretrained: bool
+    :param pretrained_backbone_name: pretrained backbone name such as
+            `'resnet50'`, `'resnet101'`, and `'mobilenet_v3_large'`
+    :type pretrained_backbone_name: str
+    :param progress: if True, displays a progress bar of the download to stderr
+    :type progress: bool
+    :param num_input_channels: number of input channels for classification head
+    :type num_input_channels: int
+    :param uses_aux: If True, add an auxiliary branch
+    :type uses_aux: bool
+    :param num_aux_channels: number of input channels for auxiliary classification head
+    :type num_aux_channels: int
+    :param return_layer_dict: mapping from name of module to return its output to a specified key
+    :type return_layer_dict: dict
+    :param num_classes: number of output classes of the model (including the background)
+    :type num_classes: int
+    :param analysis_config: analysis configuration
+    :type analysis_config: dict or None
+    :param analyzable_layer_key: key of analyzable layer
+    :type analyzable_layer_key: str
+    :param start_ckpt_file_path: checkpoint file path to be loaded for the built DeepLabv3 model
+    :type start_ckpt_file_path: str or None
+    """
     if analysis_config is None:
         analysis_config = dict()
 
--- sc2bench/models/segmentation/registry.py
+++ sc2bench/models/segmentation/registry.py
@@ -10,18 +10,42 @@ SEGMENTATION_MODEL_FUNC_DICT = dict()
 
 
 def register_segmentation_model_class(cls):
+    """
+    Registers a semantic segmentation model
+
+    :param cls: semantic segmentation model to be registered
+    :type cls: class
+    :return: cls: semantic segmentation model
+    :rtype: cls: class
+    """
     SEGMENTATION_MODEL_CLASS_DICT[cls.__name__] = cls
     register_model_class(cls)
     return cls
 
 
 def register_segmentation_model_func(func):
+    """
+    Registers a function to build a semantic segmentation
+
+    :param func: function to build a semantic segmentation to be registered
+    :type func: typing.Callable
+    :return: func: function to build a semantic segmentation
+    :rtype: func: typing.Callable
+    """
     SEGMENTATION_MODEL_FUNC_DICT[func.__name__] = func
     register_model_func(func)
     return func
 
 
 def get_segmentation_model(cls_or_func_name, **kwargs):
+    """
+    Gets a semantic segmentation model
+
+    :param cls_or_func_name: model class or function name
+    :type cls_or_func_name: str
+    :return: model: semantic segmentation model
+    :rtype: model: nn.Module or None
+    """
     if cls_or_func_name in SEGMENTATION_MODEL_CLASS_DICT:
         return SEGMENTATION_MODEL_CLASS_DICT[cls_or_func_name](**kwargs)
     elif cls_or_func_name in SEGMENTATION_MODEL_FUNC_DICT:
@@ -30,6 +54,19 @@ def get_segmentation_model(cls_or_func_name, **kwargs):
 
 
 def load_segmentation_model(model_config, device, strict=True):
+    """
+    Loads a semantic segmentation model
+
+    :param model_config: model configuration
+    :type model_config: dict
+    :param device: device
+    :type device: torch.device or str
+    :param strict: whether to strictly enforce that the keys in state_dict match
+            the keys returned by this module's `state_dict()` function.
+    :type strict: bool
+    :return: model: semantic segmentation model
+    :rtype: model: nn.Module
+    """
     model = get_semantic_segmentation_model(model_config)
     model_name = model_config['name']
     if model is None:
