ae93102facf01424af933e341439ff34b4748a24
Yoshitomo Matsubara <dr.yoshitomo.matsubara@gmail.com>
2024-09-15 16:49:02 -0700

    Update dependencies

--- setup.py
+++ setup.py
@@ -6,24 +6,24 @@ with open('README.md', 'r') as f:
 description = 'SC2 Benchmark: Supervised Compression for Split Computing.'
 setup(
     name='sc2bench',
-    version='0.0.4',
+    version='0.1.0',
     author='Yoshitomo Matsubara',
     description=description,
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/yoshitomo-matsubara/sc2-benchmark',
     packages=find_packages(exclude=('configs', 'resources', 'script', 'tests')),
-    python_requires='>=3.7',
+    python_requires='>=3.8',
     install_requires=[
-        'torch>=1.11.0,<=1.12.1',
-        'torchvision>=0.12.0,<=0.13.1',
+        'torch>=2.0.0',
+        'torchvision>=0.15.1',
         'numpy',
-        'pyyaml>=5.4.1',
+        'pyyaml>=6.0.0',
         'scipy',
         'cython',
         'pycocotools>=2.0.2',
-        'torchdistill<=0.3.3',
-        'compressai>=1.1.8,<=1.2.2',
+        'torchdistill>=1.0.0',
+        'compressai>=1.2.3',
         'timm>=0.4.12'
     ],
     extras_require={
