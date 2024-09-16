ae026c3e0cfcb46bf6e8e0cda754dc0839a0a474
Yoshitomo Matsubara <dr.yoshitomo.matsubara@gmail.com>
2024-09-15 17:30:51 -0700

    Rename

--- script/legacy/software/install_vtm.sh
+++
@@ -1,8 +0,0 @@
-mkdir ~/software
-cd ~/software
-git clone https://vcgit.hhi.fraunhofer.de/jvet/VVCSoftware_VTM.git
-cd VVCSoftware_VTM
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
